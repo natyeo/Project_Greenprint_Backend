636bcfa491482ad8a04c39ea318eada472070531
SevenSecrets <freddieshughes@googlemail.com>
2020-01-08 16:32:34 +0000

    Fix - Output in correct format: arrays within JSON

--- src/app.js
+++ src/app.js
@@ -16,24 +16,24 @@ app.use(express.urlencoded({ extended: false }));
 // Production routes
 app.post('/', (req, res) => {
   res.status(200).json({
-    walking: {
+    walking: [{
       mode: "walking",
       travel_time: "3 hours",
       distance: 8,
       carbon: 0
-    },
-    cycling: {
+    }],
+    cycling: [{
       mode: "cycling",
       travel_time: "1 hour",
       distance: 8,
       carbon: 0
-    },
-    car: {
+    }],
+    car: [{
       mode: "car",
       travel_time: "20 minutes",
       distance: 10,
       carbon: 2.30
-    }
+    }]
   })
 })
 
@@ -44,23 +44,20 @@ app.get('/', (req, res) => {
 })
 
 // Test routes
-app.get('/test-route', (req, res) => {
+app.post('/test-route', (req, res) => {
 
-  googleMaps.directions({
-    origin: 'London',
-    destination: 'Leeds',
+  googleMaps.distanceMatrix({
+    origins: 'London',
+    destinations: 'Paris',
     units: 'imperial'
   }, function(err, response) {
     if (!err) {
-        var rawDistance = response.json.routes[0].legs[0].distance.text
-         res.send("Your journey is " + rawDistance + "les long!");
-
+      var rawDistance = response.json
+      res.json(response.json);
     } else {
       console.log(err);
     }
-  }
-
-  );
+  });
 });
 
 
