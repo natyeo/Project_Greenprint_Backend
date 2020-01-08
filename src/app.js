610572827a12616848c5642a334f1e9bc6da6184
Andy Jones <andytjones@icloud.com>
2020-01-08 13:07:25 +0000

    Feature - Sends back miles of route for pre-defined query.

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+var googleMaps = require('@google/maps').createClient({
+  key: 'AIzaSyBp5iH1Pa9j2upLFHEslrir61_T7eliNNg'
+});
 
 var app = express();
 
@@ -9,14 +12,31 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.status(200).json({
-    "thing": "somethingElse"
-  })
-});
-
+// Production routes
 app.post('/', (req, res) => {
   res.status(200).json(req.body)
 })
+
+// Test routes
+app.get('/test-route', (req, res) => {
+
+  googleMaps.directions({
+    origin: 'London',
+    destination: 'Leeds',
+    units: 'imperial'
+  }, function(err, response) {
+    if (!err) {
+        var rawDistance = response.json.routes[0].legs[0].distance.text
+         res.send("Your journey is " + rawDistance + "les long!");
+         
+    } else {
+      console.log(err);
+    }
+  }
+
+  );
+});
+
+
 
 module.exports = app;
