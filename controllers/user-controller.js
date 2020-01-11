716f01903403190368ff0363518914826800f52e
elfi yang <elfiyang16@gmail.com>
2020-01-11 15:38:36 +0000

    feat: check input email

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -16,5 +16,16 @@ const { errors, isValid } = validateRegisterInput(req.body);
     return res.status(400).json(errors);
   }
 
+  User.findOne({ email: req.body.email }).then(user => {
+      if (user) {
+        return res.status(400).json({ email: "Email already exists" });
+      } else {
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password
+        });
+
+
 
 };
