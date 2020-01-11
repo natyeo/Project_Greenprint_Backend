6cc6aeab2e6d5e25dfb402cf1ff55de63e4e008a
elfi yang <elfiyang16@gmail.com>
2020-01-11 15:04:46 +0000

    feat: add validator to check names

--- validation/register.js
+++ validation/register.js
@@ -10,4 +10,9 @@ module.exports = function validateRegisterInput(data) {
   data.password = !isEmpty(data.password) ? data.password : "";
   data.password2 = !isEmpty(data.password2) ? data.password2 : "";
 
+  // Name checks
+  if (Validator.isEmpty(data.name)) {
+    errors.name = "Name field is required";
+  }
+
 };
