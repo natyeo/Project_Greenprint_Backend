f856f019ff30af844eb008e4886c1a0da981f284
elfi yang <elfiyang16@gmail.com>
2020-01-11 15:19:56 +0000

    fix: route path for validations files in user-router

--- routes/user-router.js
+++ routes/user-router.js
@@ -5,5 +5,8 @@ const jwt = require("jsonwebtoken");
 const { secretOrKey } = require('../config')
 
 // Load input validation
-const validateRegisterInput = require("../../validation/register");
-const validateLoginInput = require("../../validation/login");
+const validateRegisterInput = require("../validation/register");
+const validateLoginInput = require("../validation/login");
+
+// Load User model
+const User = require("../../models/User");
