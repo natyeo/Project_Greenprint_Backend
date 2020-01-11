1af17bae3082e0c8f7b73970357668bdd0591ce3
elfi yang <elfiyang16@gmail.com>
2020-01-11 15:07:59 +0000

    feat: convert empty fields to an empty string to use validator functions

--- validation/login.js
+++ validation/login.js
@@ -4,5 +4,8 @@ const isEmpty = require("is-empty");
 module.exports = function validateLoginInput(data) {
   let errors = {};
 
+  // Convert empty fields to an empty string so we can use validator functions
+    data.email = !isEmpty(data.email) ? data.email : "";
+    data.password = !isEmpty(data.password) ? data.password : "";
 
 };
