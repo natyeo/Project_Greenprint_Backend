359c349053da9640d9dd441fe3479f5e174de0fe
elfi yang <elfiyang16@gmail.com>
2020-01-11 15:05:51 +0000

    feat: add validator to check password

--- validation/register.js
+++ validation/register.js
@@ -22,4 +22,23 @@ module.exports = function validateRegisterInput(data) {
     errors.email = "Email is invalid";
   }
 
+  // Password checks
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+if (Validator.isEmpty(data.password2)) {
+    errors.password2 = "Confirm password field is required";
+  }
+if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = "Password must be at least 6 characters";
+  }
+if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = "Passwords must match";
+  }
+return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
+
 };
