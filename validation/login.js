18d275f3f952bd18eef5dba753d104c5affa06b4
elfi yang <elfiyang16@gmail.com>
2020-01-11 15:09:46 +0000

    feat: add validator to check emails

--- validation/login.js
+++ validation/login.js
@@ -8,4 +8,11 @@ module.exports = function validateLoginInput(data) {
     data.email = !isEmpty(data.email) ? data.email : "";
     data.password = !isEmpty(data.password) ? data.password : "";
 
+  // Email checks
+    if (Validator.isEmpty(data.email)) {
+      errors.email = "Email field is required";
+    } else if (!Validator.isEmail(data.email)) {
+      errors.email = "Email is invalid";
+    }
+
 };
