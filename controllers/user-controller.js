0a1344ff65c9bdbb4c158e005ab0e4b091359221
elfi yang <elfiyang16@gmail.com>
2020-01-11 15:34:36 +0000

    feat: import input validation into user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,1 +1,8 @@
 const User = require('../models/user-model')
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { secretOrKey } = require('../config')
+
+// Load input validation
+const validateRegisterInput = require("../validation/register");
+const validateLoginInput = require("../validation/login");
