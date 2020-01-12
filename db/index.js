745f6ca937f27cd077d373ff8a041eb1b71c5154
fahus <faduma.h@yahoo.com>
2020-01-12 14:07:10 +0000

    FEATURE: added ref to link schemas

--- db/index.js
+++ db/index.js
@@ -1,5 +1,6 @@
 const { mongo_atlas_key } = require("../config");
 
+
 const mongoose = require("mongoose");
 mongoose.connect(mongo_atlas_key, { useNewUrlParser: true }).catch(e => {
   console.error("Connection error", e.message);
