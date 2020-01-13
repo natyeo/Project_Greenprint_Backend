4def8f57a15ad9237ff620a53f5a8beb2a99c12d
fahus <faduma.h@yahoo.com>
2020-01-13 16:17:19 +0000

    cluster connection added to process env

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const app = require('./src/app');
+const db = process.env.MONGO_ATLAS_KEY;
 
 var port = process.env.PORT
 
 app.listen(port, () => {
   console.log(process.env.PORT)
+
+
+  const connectDB = async () => {
+    try {
+      await mongoose.connect(db, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+      });
+      console.log("MongoDB is Connected...");
+    } catch (err) {
+      console.error(err.message);
+      process.exit(1);
+    }
+  };
+
+
+
+
+
 });
