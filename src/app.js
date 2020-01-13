2f870fe4b9890174fcada208d0cbb63d227f825e
SevenSecrets <freddieshughes@googlemail.com>
2020-01-13 13:26:03 +0000

    Fix - added curlies to if/else https force

--- src/app.js
+++ src/app.js
@@ -11,10 +11,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 if(process.env.NODE_ENV === 'production') {
   app.use((req, res, next) => {
-    if (req.header('x-forwarded-proto') !== 'https')
+    if (req.header('x-forwarded-proto') !== 'https') {
       res.redirect(`https://${req.header('host')}${req.url}`)
-    else
+    } else {
       next()
+    }
   })
 }
 
