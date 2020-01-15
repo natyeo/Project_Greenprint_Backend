a4f45a6c4055b123b206351d56368ea79c9b08b5
SevenSecrets <freddieshughes@googlemail.com>
2020-01-15 11:28:33 +0000

    Test - creating test route

--- src/app.js
+++ src/app.js
@@ -30,79 +30,89 @@ if(process.env.NODE_ENV === 'production') {
   app.use((req, res, next) => {
     if (req.header('x-forwarded-proto') !== 'https') {
       res.redirect(`https://${req.header('host')}${req.url}`)
-    } else {
-      next()
-    }
-  })
-}
-
-// Production routes
-app.post('/', (req, res) => {
-  const driving = Api.googleApiCall(req, 'driving')
-  const transit = Api.googleApiCall(req, 'transit')
-  const bicycling = Api.googleApiCall(req, 'bicycling')
-  const walking = Api.googleApiCall(req, 'walking')
-
-  Promise.all([driving, transit, walking, bicycling]).then((values) => {
-    const results = values;
-    let drivingDistance
-    let transitDistance
-
-    results.filter(function(item){
-      item.mode == 'driving' ? drivingDistance = item.distance.slice(0, -3) : drivingDistance;
-      item.mode == 'transit' ? transitDistance = item.distance.slice(0, -3) : transitDistance;
+      } else {
+        next()
+      }
     })
+  }
 
-    results.forEach(function(item, i){
-      item.distance = item.distance.slice(0, -3)
+  // Production routes
+  app.post('/', (req, res) => {
+    const driving = Api.googleApiCall(req, 'driving')
+    const transit = Api.googleApiCall(req, 'transit')
+    const bicycling = Api.googleApiCall(req, 'bicycling')
+    const walking = Api.googleApiCall(req, 'walking')
+
+    Promise.all([driving, transit, walking, bicycling]).then((values) => {
+      const results = values;
+      let drivingDistance
+      let transitDistance
+
+      results.filter(function(item){
+        item.mode == 'driving' ? drivingDistance = item.distance.slice(0, -3) : drivingDistance;
+        item.mode == 'transit' ? transitDistance = item.distance.slice(0, -3) : transitDistance;
+      })
+
+      results.forEach(function(item, i){
+        item.distance = item.distance.slice(0, -3)
+      })
+
+      const carUrl = `https://api.triptocarbon.xyz/v1/footprint?activity=${drivingDistance}&activityType=miles&country=def&mode=anyCar&appTkn=${carbon_key}`
+      const transitUrl = `https://api.triptocarbon.xyz/v1/footprint?activity=${transitDistance}&activityType=miles&country=def&mode=transitRail&appTkn=${carbon_key}`
+
+      Api.returnFinalResponse(results, carUrl, transitUrl, res)
     })
-
-    const carUrl = `https://api.triptocarbon.xyz/v1/footprint?activity=${drivingDistance}&activityType=miles&country=def&mode=anyCar&appTkn=${carbon_key}`
-    const transitUrl = `https://api.triptocarbon.xyz/v1/footprint?activity=${transitDistance}&activityType=miles&country=def&mode=transitRail&appTkn=${carbon_key}`
-
-    Api.returnFinalResponse(results, carUrl, transitUrl, res)
+    .catch(err => {
+      console.log(err)
+      return res.status(400).send({
+        error: "Bad Request",
+        description: "Have you considered entering more specific locations?"
+      })
+    });
   })
-  .catch(err => {
-    console.log(err)
-    return res.status(400).send({
-      error: "Bad Request",
-      description: "Have you considered entering more specific locations?"
+
+  // Test routes
+  app.post('/test-route', (req, res) => {
+    res.status(200).json([
+      {
+        mode: "walking",
+        travel_time: "3 hours",
+        distance: 8,
+        carbon: 0
+      },
+      {
+        mode: "bicycling",
+        travel_time: "1 hour",
+        distance: 8,
+        carbon: 0
+      },
+      {
+        mode: "driving",
+        travel_time: "20 minutes",
+        distance: 10,
+        carbon: 2.30
+      },
+      {
+        mode: "transit",
+        travel_time: "10 minutes",
+        distance: 2,
+        carbon: 0.5
+      }
+    ])
+  });
+
+  app.post('/flights/test-route', (req, res) => {
+    res.status(200).json({
+      mode: "flying",
+      origin: "Sydney Bankstown",
+      destination: "London City",
+      distance: 6482,
+      carbon: 10000
     })
   });
-})
 
-// Test routes
-app.post('/test-route', (req, res) => {
-  res.status(200).json({ results: [
-    {
-      mode: "walking",
-      travel_time: "3 hours",
-      distance: 8,
-      carbon: 0
-    },
-    {
-      mode: "bicycling",
-      travel_time: "1 hour",
-      distance: 8,
-      carbon: 0
-    },
-    {
-      mode: "driving",
-      travel_time: "20 minutes",
-      distance: 10,
-      carbon: 2.30
-    },
-    {
-      mode: "transit",
-      travel_time: "10 minutes",
-      distance: 2,
-      carbon: 0.5
-    }
-  ]})
-});
-
-app.use('/travel', recordRouter)
-app.use('/user', userRouter)
+  app.use('/travel', recordRouter)
+  app.use('/user', userRouter)
 
 
-module.exports = app;
+  module.exports = app;
