8741f9210ce3ff6eca1f21ec6f75616485453bc8
SevenSecrets <freddieshughes@googlemail.com>
2020-01-16 13:27:21 +0000

    Test - Flight route testing

--- test/flightroute.test.js
+++ test/flightroute.test.js
@@ -5,8 +5,8 @@ const app = require('../src/app');
 chai.use(chaiHttp);
 chai.should();
 
-describe('flights post', () => {
-  it('should return a 200 code', (done) => {
+describe('flights route post requests', () => {
+  it('should return a 200 code', () => {
     chai.request(app)
     .post('/flights')
     .type('form')
@@ -14,9 +14,45 @@ describe('flights post', () => {
       "from": "DXB",
       "to": "SYD"
     })
-    .then((err, res) => {
+    .then(function(res) {
       expect(res).to.have.status(200);
-      done();
+    })
+    .catch(function(err) {
+      throw err;
     });
   });
+
+  it('should return an object', () => {
+    chai.request(app)
+    .post('/flights')
+    .type('form')
+    .send({
+      "from": "DXB",
+      "to": "SYD"
+    })
+    .then(function(res) {
+      expect(res).to.be.a('object');
+    })
+    .catch(function(err) {
+      throw err;
+    })
+  })
+
+  it('should return the right fields', () => {
+    chai.request(app)
+    .post('/flights')
+    .type('form')
+    .send({
+      "from": "DXB",
+      "to": "SYD"
+    })
+    .then((res) => {
+      expect(res).to.include({"mode": "flying"})
+      expect(res).to.include({"origin": "Dubai International"})
+      expect(res).to.include({"destination": "Sydney International"})
+      expect(res).to.include({"distance": "4028"})
+      expect(res).to.include({"carbon": "1957.02"})
+      expect(res).to.include({"barrels_of_oil": "4.55"})
+    })
+  })
 });
